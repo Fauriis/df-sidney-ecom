4d54d8b9215b7b35508dbac6bb48e716d603279d
Fauriis <dobreflorentina58@gmail.com>
2022-09-26 21:04:20 +0300

    com

---
+++ src/pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const Homepage = () => {
+  return (
+    <>
+      <Head>
+        <title>Contact Page</title>
+      </Head>
+
+      <main>hello from next</main>
+    </>
+  );
+};
+
+export default Homepage;
+// obligatoriu returnat un default export
