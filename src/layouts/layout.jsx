043ebeb74a7942c8d9dd2013e5fd99726b7a28a1
Florentina Dobre <97975615+Fauriis@users.noreply.github.com>
2022-09-28 20:05:19 +0300

    .

--- src/layouts/layout.jsx
+++ src/layouts/layout.jsx
@@ -1,3 +1,32 @@
+import { css } from '@emotion/css';
+
+const black = '#101010';
+const white = '#fff'
+
 export const Layout = ({ children }) => {
-  return <>{children}</>;
+  const gridCss = css `display: css;
+  grid-template-areas:
+  'header'
+  'main-area';
+
+  grid-template-rows: 40px 1fr;
+  `
+const headerCss = css `background-color: ${black};
+color: ${white};
+`
+
+  return (
+    <div className={gridCss}>
+    <header className={headerCss} >x</header>
+
+    <div>
+    {children}
+    <footer>x</footer>
+    </div>
+    </div>
+  );
 };
+
+
+
+
