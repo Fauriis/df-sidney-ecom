79d01d0c17295faca661ed896b023ed92a822558
Fauriis <dobreflorentina58@gmail.com>
2022-12-04 13:16:13 +0200

    app

--- src/components/catalog/InfinitePagination.jsx
+++ src/components/catalog/InfinitePagination.jsx
@@ -1,62 +1,62 @@
-import { useEffect, useRef, useState } from 'react';
-const options = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 1.0,
-};
+// import { useEffect, useRef, useState } from 'react';
+// const options = {
+//   root: null,
+//   rootMargin: '0px',
+//   threshold: 1.0,
+// };
 
-export const InfinitePagination = ({ products, setPaginatedProducts }) => {
-  const [pagination, setPagination] = useState({
-    perPage: 8,
-    page: 0,
-    total: 0,
-  });
+// export const InfinitePagination = ({ products, setPaginatedProducts }) => {
+//   const [pagination, setPagination] = useState({
+//     perPage: 8,
+//     page: 0,
+//     total: 0,
+//   });
 
-  const loadMoreRef = useRef(null);
+//   const loadMoreRef = useRef(null);
 
-  // console.log(loadMoreRef.current);
+//   // console.log(loadMoreRef.current);
 
-  useEffect(() => {
-    setPagination({
-      perPage: pagination.total,
-      page: pagination.perPage,
-      total: products.length,
-    });
-  }, [products, pagination.perPage, pagination.page, setPagination]);
+//   useEffect(() => {
+//     setPagination({
+//       perPage: pagination.total,
+//       page: pagination.perPage,
+//       total: products.length,
+//     });
+//   }, [products, pagination.perPage, pagination.page, setPagination]);
 
-  useEffect(() => {
-    const { total, page, perPage } = pagination;
+//   useEffect(() => {
+//     const { total, page, perPage } = pagination;
 
-    if (total === 0 || page < 1) {
-      return;
-    }
+//     if (total === 0 || page < 1) {
+//       return;
+//     }
 
-    const newProducts = products.slice().splice(perPage * (page - 1), perPage);
+//     const newProducts = products.slice().splice(perPage * (page - 1), perPage);
 
-    setPaginatedProducts(newProducts);
-  }, [pagination, setPaginatedProducts, products]);
+//     setPaginatedProducts(newProducts);
+//   }, [pagination, setPaginatedProducts, products]);
 
-  useEffect(() => {
-    const loadMoreEdge = loadMoreRef.current;
-    const observer = new IntersectionObserver((entries) => {
-      const [intersectionObserverEntry] = entries;
+//   useEffect(() => {
+//     const loadMoreEdge = loadMoreRef.current;
+//     const observer = new IntersectionObserver((entries) => {
+//       const [intersectionObserverEntry] = entries;
 
-      if (intersectionObserverEntry.isIntersecting && pagination.total !== 0) {
-        setPagination({
-          ...pagination,
-          page: pagination.page + 1,
-        });
-      }
-    }, options);
+//       if (intersectionObserverEntry.isIntersecting && pagination.total !== 0) {
+//         setPagination({
+//           ...pagination,
+//           page: pagination.page + 1,
+//         });
+//       }
+//     }, options);
 
-    observer.observe(loadMoreEdge);
+//     observer.observe(loadMoreEdge);
 
-    return () => {
-      observer.unobserve(loadMoreEdge);
-    };
-  }, [pagination.total, pagination.page, pagination.perPage]);
+//     return () => {
+//       observer.unobserve(loadMoreEdge);
+//     };
+//   }, [pagination.total, pagination.page, pagination.perPage]);
 
-  return (
-    <div ref={loadMoreRef} className="border border-t border-pink-500"></div>
-  );
-};
+//   return (
+//     <div ref={loadMoreRef} className="border border-t border-pink-500"></div>
+//   );
+// };
